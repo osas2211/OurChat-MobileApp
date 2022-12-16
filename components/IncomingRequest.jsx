1a5193c233270b1314a67abb5bc56e712f86a91d
Osaretin Frank <osaretin.frank10@gmail.com>
2022-12-16 13:59:30 +0100

    added notification screen

---
+++ components/IncomingRequest.jsx
@@ -0,0 +1,103 @@
+import { View, Text, Image, TouchableOpacity } from "react-native"
+import React from "react"
+import { assets, colors } from "../constants"
+
+export const IncomingRequest = ({ image, name, id }) => {
+  return (
+    <View
+      style={{
+        width: "90%",
+        flexDirection: "row",
+        marginVertical: 7,
+        alignSelf: "center",
+        padding: 10,
+        borderColor: "#CCDBDC",
+        borderWidth: 1,
+        borderRadius: 8,
+        alignItems: "center",
+      }}
+    >
+      <TouchableOpacity>
+        <Image
+          source={image}
+          resizeMode="contain"
+          style={{
+            width: 50,
+            height: 50,
+            borderRadius: 50,
+            borderColor: "#CCDBDC",
+            borderWidth: 1,
+          }}
+        />
+      </TouchableOpacity>
+      <View style={{ marginLeft: 10 }}>
+        <View>
+          <Text style={{ textTransform: "capitalize", fontFamily: "MontBold" }}>
+            {name}
+          </Text>
+          <Text style={{ fontFamily: "MontLight" }}>
+            Sent a friend request.
+          </Text>
+        </View>
+        <View
+          style={{
+            marginTop: 8,
+            flexDirection: "row",
+          }}
+        >
+          <ActionButton
+            bgColor={colors.mintGreen}
+            color={colors.babyPowder}
+            text={"accept"}
+            onPress={() => {}}
+          />
+          <ActionButton
+            bgColor={"#CCDBDC"}
+            color={colors.smokyBlack}
+            text={"reject"}
+            onPress={() => {}}
+          />
+        </View>
+      </View>
+      <Text
+        style={{
+          position: "absolute",
+          top: 8,
+          right: 15,
+          fontFamily: "MontLight",
+          fontSize: 12,
+          color: colors.smokyBlack,
+        }}
+      >
+        1hr
+      </Text>
+    </View>
+  )
+}
+
+export const ActionButton = ({ bgColor, color, text, onPress }) => {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <View
+        style={{
+          backgroundColor: bgColor,
+          padding: 4,
+          borderRadius: 5,
+          width: 120,
+          marginRight: 10,
+        }}
+      >
+        <Text
+          style={{
+            textAlign: "center",
+            color: color,
+            textTransform: "capitalize",
+            fontFamily: "MontMedium",
+          }}
+        >
+          {text}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  )
+}
--- screens/TabScreens/Notifications.jsx
+++ screens/TabScreens/Notifications.jsx
@@ -2,11 +2,26 @@ import { View, Text } from "react-native"
 import React from "react"
 import { TabStyles } from "./TabSyles"
 import { TabHeader } from "../../components/TabHeader"
+import { assets, colors } from "../../constants"
+import { IncomingRequest } from "../../components/IncomingRequest"
 
 export const Notifications = ({ navigation }) => {
   return (
     <View style={TabStyles.container}>
       <TabHeader tabName={"notifications"} navigation={navigation} />
+      <View
+        style={{
+          width: "100%",
+          justifyContent: "center",
+          marginTop: 15,
+        }}
+      >
+        <IncomingRequest name={"justin ryhes"} image={assets.profile} />
+        <IncomingRequest name={"justin ryhes"} image={assets.stewie} />
+        <IncomingRequest name={"justin ryhes"} image={assets.authImg} />
+        <IncomingRequest name={"justin ryhes"} image={assets.authImg2} />
+        <IncomingRequest name={"justin ryhes"} image={assets.authImg4} />
+      </View>
     </View>
   )
 }
